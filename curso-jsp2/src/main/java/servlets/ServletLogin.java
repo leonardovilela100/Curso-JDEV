7ff71f6facca4aca0a42ff8fd2114285cfe013cd
Leonardo Carvalho <leovilela100@gmail.com>
2023-03-20 21:18:35 -0300

    projetojsf

---
+++ curso-jsp2/src/main/java/servlets/ServletLogin.java
@@ -0,0 +1,92 @@
+package servlets;
+
+import java.io.IOException;
+
+import dao.DAOLoginRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.ModelLogin;
+
+// The call Controller are the Servelets or ServeletLoginController
+@WebServlet(urlPatterns = { "/principal/ServletLogin", "/ServletLogin" }) // URL mapping coming from screen
+public class ServletLogin extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private DAOLoginRepository daoLoginRepository = new DAOLoginRepository();
+
+	public ServletLogin() {
+	}
+
+	// Receive data by URL in parameters
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+					
+		String acao = request.getParameter("acao");
+		
+		if(acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("logout")) {
+			request.getSession().invalidate();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			redirecionar.forward(request, response);
+		}else {
+			doPost(request, response);
+		}
+		
+		
+
+	}
+
+	// Receives data sent by a form
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		String url = request.getParameter("url");
+
+		try {
+
+			if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
+
+				ModelLogin modelLogin = new ModelLogin();
+				modelLogin.setLogin(login);
+				modelLogin.setSenha(senha);
+
+				if (daoLoginRepository.validarAutenticacao(modelLogin)) { /* Simulando login */
+
+					request.getSession().setAttribute("usuario", modelLogin.getLogin());
+
+					if (url == null || url.equals("null")) {
+						url = "principal/principal.jsp";
+					}
+
+					url = "principal/principal.jsp";
+
+					RequestDispatcher redirecionar = request.getRequestDispatcher(url);
+					redirecionar.forward(request, response);
+
+				} else {
+					RequestDispatcher redirecionar = request.getRequestDispatcher("/index.jsp");
+					request.setAttribute("msg", "Informe o login e senha corretamente!");
+					redirecionar.forward(request, response);
+				}
+
+			} else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe o login e senha corretamente!");
+				redirecionar.forward(request, response);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+			
+		}
+
+	}
+
+}
